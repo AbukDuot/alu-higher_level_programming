3bca7b317fdc409197e5798b6cdbe5c812ad200b
AbukDuot <a.duot@alustudent.com>
2023-07-27 05:28:30 -0700

    6-base_geometry.py

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""defines class BaseGeometry with public instance method for area"""
+
+
+class BaseGeometry:
+    """class withn public instance method ti raise exception"""
+    def area(self):
+        raise Exception("area() is not immmplemnted")
