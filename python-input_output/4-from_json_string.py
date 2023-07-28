d17a18f5df00c95084e525c6cced20337297baf5
AbukDuot <a.duot@alustudent.com>
2023-07-28 00:50:35 -0700

    4-from_json_string.py

--- python-input_output/4-from_json_string.py
+++ python-input_output/4-from_json_string.py
@@ -1,18 +1,8 @@
 #!/usr/bin/python3
-"""
-    6-load_from_json_file: load_from_json_file()
-"""
+"""defines function to return object from JSON"""
 
 
-import json
-
-
-def load_from_json_file(filename):
-    """
-        loads an object to from JSON file.
-        Args:
-            filename (str): name of file.
-    """
-    with open(filename, "r") as j_file:
-        my_obj = json.load(j_file)
-        return my_obj
+def from_json_string(my_str):
+    """returns object from JSON"""
+    import json
+    return json.loads(my_str)
